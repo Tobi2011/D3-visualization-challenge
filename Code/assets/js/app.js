dde5ae2b11b76fbc1b724bea8fad89218c1896e9
Tobi <jandrade@cssflorida.net>
2019-05-13 16:52:02 -0400

    level 2 is almost done

--- Code/assets/js/app.js
+++ Code/assets/js/app.js
@@ -1,107 +1,249 @@
 //width and height
 var w = 800;
-var h = 500;
-var padding = 50;
+var h = 600;
+var padding = 100;
+
+
 
 // /*
 //load data
-d3.csv('assets/data/data.csv').then(function(dataset) {
- 
-	dataset.forEach(function (d) {
-		d.poverty = +d.poverty;
-		d.healthcare = +d.healthcare;
-		d.abbr = String(d.abbr);
-		console.log(d.abbr)
-	}); 
+//This opens the csv and leaves it open to work with the data
+d3.csv('assets/data/data.csv').then(function (dataset) {
+	//dropping columns array
+	delete dataset["columns"];
+	//adds variability for options in x and y axis
+	var xValues = "poverty";
+	var yValues = "healthcare";
+	var toolTipState = "state";
 
-console.log(dataset);	
-		
+	var svg = d3.select("#scatter")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+	
+	// d3.selectAll("svg > *").remove();
+
+			
+	function scatterPlot(xValues, yValues) {
+
+		// This typecasts the data being used properly
+		dataset.forEach(function (d) {
+			d[xValues] = +d[xValues];
+			d[yValues] = +d[yValues];
+			d.abbr = String(d.abbr);
+			// console.log(typeof d[xValues])
+		});
+		//print all data to console
+		console.log(dataset);
 		//scale function
+		var tickNumber = 10;
+		var xValPadding = d3.max(dataset, function (d) { return d[xValues]; }) / d3.min(dataset, function (d) { return d[xValues]; });
+		var yValPadding = d3.max(dataset, function (d) { return d[yValues]; }) / d3.max(dataset, function (d) { return d[yValues]; });
+		//These variables help with scaling the x and y axis to match the data
 		var xScale = d3.scaleLinear()
-			//.domain(["Alabama","Alaska","Arizona","Arkansas","California"])
-			.domain([d3.min(dataset, function (d) { return d.healthcare; }) + 3,
-								d3.max(dataset, function (d) { return d.poverty; }) + 1])
-			//.range([padding, w-padding * 2]);
+			.domain([d3.min(dataset, function (d) { return d[xValues]; }) - xValPadding,
+			d3.max(dataset, function (d) { return d[xValues]; }) + xValPadding])
 			.range([padding, w - padding * 2]);
 			
 		var yScale = d3.scaleLinear()
-			.domain([d3.min(dataset, function (d) { return d.healthcare; }) - 1,
-								d3.max(dataset, function (d) { return d.healthcare; }) + 1 ])
-			//.range([padding, w-padding * 2]);
+			.domain([d3.min(dataset, function (d) { return d[yValues]; }) - yValPadding,
+			d3.max(dataset, function (d) { return d[yValues]; }) + yValPadding])
 			.range([h - padding, padding]);
+		// Axes
+		//These variables set the x and y axis into the svg
+		var xAxis = d3.axisBottom().scale(xScale).ticks(tickNumber);
 		
-		var xAxis = d3.axisBottom().scale(xScale).ticks(10);
-		
-		var yAxis = d3.axisLeft().scale(yScale).ticks(10);
+		var yAxis = d3.axisLeft().scale(yScale).ticks(tickNumber);
 		
 		//create svg element
-		var svg = d3.select("#scatter")
-					.append("svg")
-					.attr("width", w)
-					.attr("height", h);
-		//circles 
+	
+		// ToolTip 
+		//pop up when mouse over each circle
+		var tool_tip = d3.tip()
+			.attr("class", "d3-tip")
+			.offset([-8, 0])
+			.html(function (d) { return d[toolTipState] + "<br>" + xValues + ": " + d[xValues] + "<br>" + yValues + ": " + d[yValues]; });
+		svg.call(tool_tip);
+		console.log(tool_tip.show);
+	
+		// function handleMouseOver() {
+		// 	d3.select(this).attr("stroke", "red");
+		// };
+	
+		// function handleMouseOut() {
+		// 	d3.select(this).attr("stroke", "white");
+		// };
+
+		//CIRCLES
+
 		svg.selectAll("circle")
 			.data(dataset)
 			.enter()
 			.append("circle")
+			.attr("class", "circle")
 			.attr("cx", function (d) {
-				return xScale(d.poverty);
+				return xScale(d[xValues]);
 			})
-			.attr("cy", function(d) {
-				return  yScale(d.healthcare);
+			.attr("cy", function (d) {
+				return yScale(d[yValues]);
 			})
-			.attr("r", 10)
+			.attr("r", 8)
 			.attr("fill", "blue")
-			.attr("stroke","white");
-	//state abbreviations 
-	svg.selectAll("state")
-		.data(dataset)
-		.enter()
-		.append("text")
-		.attr("x", function (d) {
-			return xScale(d.poverty);
-		})
-		.attr("y", function (d) {
-			// console.log(yScale(d.healthcare));
-			return  yScale(d.healthcare);
-		})
-		.text(function (d) {
-			return d.abbr;
-		} )
-		.attr("font-size", "7px")
-		.attr("fill", "white")
-		.attr("text-anchor", "middle")
-		// location should be relative to circle radius
-		.attr("transform",'translate(0, 2.5)')
+			.attr("stroke", "white")
+			.on("mouseover", tool_tip.show)
+			.on('mouseout', tool_tip.hide);
+	
+		
+		//TEXT : state abbreviations 
+		svg.selectAll("text")
+			.data(dataset)
+			.enter()
+			.append("text")
+			.attr("class", "abbr")
+			.attr("x", function (d) {
+				// console.log(d);
+				return xScale(d[xValues]);
+			})
+			.attr("y", function (d) {
+				return yScale(d[yValues]);
+			})
+			.text(function (d) {
+				return d.abbr;
+			})
+			.attr("font-size", "7px")
+			.attr("fill", "red")
+			.attr("text-anchor", "middle")
+			// location should be relative to circle radius
+			.attr("transform", 'translate(0, 2.5)')
+			.attr("pointer-events", "none");
 		//x axis
+
 		svg.append("g")
-			.attr("class", "x axis")	
+			.attr("class", "xaxis")
 			.attr("transform", "translate(0," + (h - padding) + ")")
 			.call(xAxis);
-		//x axis label
-		svg.append("text")
-			.text("In Poverty (%)")
-			.attr("font-size", "10px")
-			.attr("font-weight", "bold")
-			.attr("fill", "black")
-			.attr("transform", "translate("+ w/2 +"," + (h - padding + 30) + ")")
-			.style("text-anchor", "middle");
-
-	
+		
 		//y axis
 		svg.append("g")
-			.attr("class", "y axis")	
-			.attr("transform", "translate(" + padding + ", 0)")
-			.call(yAxis);
+		.attr("class", "yaxis")	
+		.attr("transform", "translate(" + padding + ", 0)")
+		.call(yAxis);
+	};
+	
+	//LABELS
+	
+		//x axis label
+	xLabelPadding = 40;
+	xLabelSeparator = 20;
+	
+	//In Poverty Label
+	svg.append("text")
+		.attr("class", "poverty")
+		.text("In Poverty (%)")
+		.attr("id", "inPoverty")
+		.attr("font-size", "13px")
+		.attr("font-weight", "bold")
+		.attr("fill", "black")
+		.attr("transform", "translate(" + w / 2 + "," + (h - padding + xLabelPadding) + ")")
+		.style("text-anchor", "middle")
+		.on("click", function () {
+			xValues = "poverty";
+			d3.selectAll(".circle").remove();
+			d3.selectAll(".abbr").remove();
+			d3.selectAll(".xaxis").remove();
+			scatterPlot(xValues, yValues)
+			});
+
+	//Age Label
+	svg.append("text")
+		.attr("class", "age")
+		.text("Age (Median)")
+		.attr("id", "age")
+		.attr("font-size", "13px")
+		.attr("font-weight", "bold")
+		.attr("fill", "black")
+		.attr("transform", "translate(" + w / 2 + "," + (h - padding + xLabelPadding + xLabelSeparator) + ")")
+		.style("text-anchor", "middle")
+		.on("click", function () {
+			xValues = "age";
+				d3.selectAll(".circle").remove();
+			d3.selectAll(".abbr").remove();
+			d3.selectAll(".xaxis").remove();
+			scatterPlot(xValues, yValues);
+			});
+
+	//Household Income Label
+	svg.append("text")
+		.attr("class", "income")
+		.text("Household Income (Median)")
+		.attr("id", "householdIncome")
+		.attr("font-size", "13px")
+		.attr("font-weight", "bold")
+		.attr("fill", "black")
+		.attr("transform", "translate(" + w / 2 + "," + (h - padding + xLabelPadding + xLabelSeparator * 2) + ")")
+		.style("text-anchor", "middle")
+		.on("click", function () {
+			xValues = "income";
+				d3.selectAll(".circle").remove();
+			d3.selectAll(".abbr").remove();
+			d3.selectAll(".xaxis").remove();
+			scatterPlot(xValues, yValues)
+			});
 	
 			//y axis label
-			svg.append("text")
-			.text("Lacks Healthcare (%)")
-			.attr("font-size", "10px")
-			.attr("font-weight", "bold")
-			.attr("fill", "black")
-			.attr("transform", "translate("+ padding/2 +"," + (h - padding + 30)/2 + ")"+" rotate(-90)")
-			// .attr("transform", "rotate(-90)")
-			.style("text-anchor", "middle");
+			yLabelPadding = 60;
+			yLabelSeparator = 20;
+	//Lacks Healthcare Label
+	svg.append("text")
+		.attr("class", "healthcare")
+		.text("Lacks Healthcare (%)")
+		.attr("font-size", "13px")
+		.attr("font-weight", "bold")
+		.attr("fill", "black")
+		.attr("transform", "translate(" + yLabelPadding + "," + (h) / 2 + ")" + " rotate(-90)")
+		.style("text-anchor", "middle")		
+		.on("click", function () {
+			yValues = "healthcare";
+				d3.selectAll(".circle").remove();
+			d3.selectAll(".abbr").remove();
+			d3.selectAll(".yaxis").remove();
+			scatterPlot(xValues, yValues)
+			});
+
+	//Smokes Label		
+	svg.append("text")
+		.attr("class", "smokes")
+		.text("Smokes (%)")
+		.attr("font-size", "13px")
+		.attr("font-weight", "bold")
+		.attr("fill", "black")
+		.attr("transform", "translate(" + (yLabelPadding - yLabelSeparator) + "," + (h) / 2 + ")" + " rotate(-90)")
+		.style("text-anchor", "middle")
+		.on("click", function () {
+			yValues = "smokes";
+				d3.selectAll(".circle").remove();
+			d3.selectAll(".abbr").remove();
+			d3.selectAll(".yaxis").remove();
+			scatterPlot(xValues, yValues)
+			});
+
+	// Obese Label
+	svg.append("text")
+		.attr("class", "obesity")
+		.text("Obese (%)")
+		.attr("font-size", "13px")
+		.attr("font-weight", "bold")
+		.attr("fill", "black")
+		.attr("transform", "translate(" + (yLabelPadding - yLabelSeparator * 2) + "," + (h) / 2 + ")" + " rotate(-90)")
+		.style("text-anchor", "middle")
+		.on("click", function () {
+			yValues = "obesity";
+				d3.selectAll(".circle").remove();
+			d3.selectAll(".abbr").remove();
+			d3.selectAll(".yaxis").remove();
+			scatterPlot(xValues, yValues)
+			});
+	
+	scatterPlot(xValues,yValues)
 });	
 		
